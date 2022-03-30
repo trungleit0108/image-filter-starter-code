e014d7b2ba87bf6996123e9954db5067952c2a98
Le Minh Trung <58909930+trunglm2@users.noreply.github.com>
2022-03-31 02:04:17 +0700

    Create EndPoint to filter image from public url (#2)
    * create new endpoint to filter, download and remove image
    
    * add evidence
    
    * ignore folder build
    
    * add axios to package.json
    
    * change other way to read image url
    
    Co-authored-by: TrungMinhLe <trung.minhle@coxautoinc.com>

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -26,21 +26,31 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage?:image_url", async (req: Request, res: Response) => {
+    try {
+      const image_url: string = req.query.image_url;
+      if (!image_url) {
+        return res.status(400).send("URL is require!");
+      }
 
-  /**************************************************************************** */
+      const pathImage: string = await filterImageFromURL(image_url);
+      res.status(200).sendFile(pathImage);
+      res.on("finish", () => deleteLocalFiles([pathImage]));
+    } catch (e) {
+      console.log(e);
+      return res.status(500).send({ error: "Unable to process your request" });
+    }
+  });
 
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (_: Request, res: Response) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
