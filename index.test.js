30d6f51159ad4242b4ea26afe0406440b88d885c
Le Minh Trung <trungleit0108@gmail.com>
2022-10-03 00:43:52 +0700

    a

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@ test("adds 1 + 2 to equal 3", () => {
   expect(2).toBe(3);
 });
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(true).toBe(true);
-});
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(true).toBe(true);
+// });
